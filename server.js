540710f2504b91ac60d9100d82501fd77f7f0e2a
Teddy Sahr <tedward1218@gmail.com>
2021-11-10 00:04:30 -0500

    added route to db.json file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require("express");
 const path = require("path");
+const data = require("./db/db.json");
 
 // SET UP APP & PORT
 const app = express();
@@ -15,6 +16,8 @@ app.get("/helloworld", (req, res) => res.send("HELLO WORLD"));
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
+
+app.get("/api/notes", (req, res) => res.json(data));
 
 // START THE SERVER
 app.listen(PORT, () =>
