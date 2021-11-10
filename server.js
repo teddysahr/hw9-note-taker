6636564c047831a01ecf2dc3a5073e5397ccaeee
Teddy Sahr <tedward1218@gmail.com>
2021-11-09 23:48:29 -0500

    connected notes html with start button

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.static("public"));
 // ROUTES
 app.get("/helloworld", (req, res) => res.send("HELLO WORLD"));
 
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
 // START THE SERVER
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
