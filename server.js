26a143439f301279d0b27c5fd3ec767e6b3a50b6
Teddy Sahr <tedward1218@gmail.com>
2021-11-09 23:18:07 -0500

    set app & port and middlewares

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // DEPENDENCIES
 const express = require("express");
 const path = require("path");
+
+// SET UP APP & PORT
 const app = express();
 const PORT = 3001;
+
+// MIDDLEWARES
+app.use(express.static("public"));
