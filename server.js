84a828f13c80bb20561f92c56748d1f1cbb9f2ef
Teddy Sahr <tedward1218@gmail.com>
2021-11-09 23:29:37 -0500

    creates routes and started server

--- server.js
+++ server.js
@@ -8,3 +8,11 @@ const PORT = 3001;
 
 // MIDDLEWARES
 app.use(express.static("public"));
+
+// ROUTES
+app.get("/helloworld", (req, res) => res.send("HELLO WORLD"));
+
+// START THE SERVER
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
