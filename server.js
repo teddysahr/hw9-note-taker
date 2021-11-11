4b4054c5e6e0f5e56a1c50a94c42f1b8535e3842
Teddy Sahr <tedward1218@gmail.com>
2021-11-10 23:52:32 -0500

    added get, post, and delete routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const path = require("path");
 const data = require("./db/db.json");
+const uuid = require("./helpers/uuid");
+const fs = require("fs");
 
 // SET UP APP & PORT
 const app = express();
@@ -10,7 +12,7 @@ const PORT = 3001;
 // MIDDLEWARES
 app.use(express.static("public"));
 
-// ROUTES
+// GET ROUTES
 app.get("/helloworld", (req, res) => res.send("HELLO WORLD"));
 
 app.get("/notes", (req, res) =>
@@ -18,6 +20,76 @@ app.get("/notes", (req, res) =>
 );
 
 app.get("/api/notes", (req, res) => res.json(data));
+
+app.get("/api/notes/:id", (req, res) => {
+  const requestedNote = req.params.id.toLowerCase();
+
+  for (let i = 0; i < data.length; i++) {
+    if (requestedNote === data[i].id.toLowerCase()) {
+      return res.json(data[i]);
+    }
+  }
+
+  // Return a message if the term doesn't exist in our DB
+  return res.json("No note found");
+});
+
+// POST ROUTES
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      review_id: uuid(),
+    };
+
+    // Convert the data to a string so we can save it
+    //const reviewString = JSON.stringify(newReview);
+
+    const noteArray = JSON.parse(fs.readFileSync(`./db/db.json`));
+
+    console.log(noteArray);
+
+    noteArray.push(newNote);
+
+    // Write the string to a file
+    fs.writeFile(`./db/db.json`, JSON.stringify(noteArray), (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Review for ${newNote.title} has been written to JSON file`
+          )
+    );
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting review");
+  }
+});
+
+// DELETE ROUTES
+app.delete("/api/notes/:id", (req, res) => {
+  const deleteNote = req.params.id.toLowerCase();
+
+  for (let i = 0; i < data.length; i++) {
+    if (deleteNote === data[i].id.toLowerCase()) {
+      return res.json(data[i]);
+    }
+  }
+
+  // Return a message if the term doesn't exist in our DB
+  return res.json("No note found");
+});
 
 // START THE SERVER
 app.listen(PORT, () =>
